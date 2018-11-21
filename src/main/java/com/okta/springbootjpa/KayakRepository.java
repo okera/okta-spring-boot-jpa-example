617b2d05f5c1ff65693dac362cf873b04c58996a
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-11-21 10:08:38 -0800

    added rest dependencies

--- src/main/java/com/okta/springbootjpa/KayakRepository.java
+++ src/main/java/com/okta/springbootjpa/KayakRepository.java
@@ -1,10 +1,12 @@
 package com.okta.springbootjpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+@RepositoryRestResource
 public interface KayakRepository extends CrudRepository<Kayak, Integer> {
 
 }
