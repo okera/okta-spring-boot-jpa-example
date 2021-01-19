5e410a4e95a7c064a3f0e32e0142b50ceb3bac80
Nong Li <nongli@gmail.com>
2021-01-18 22:02:55 -0800

    [okera] Implement initial tutorial
    Note that this sets a dummy 'tutorial' password for postgres. This is not
    safe if postgres is open on the network.
    
    To start the server, run
      ./gradlew bootRun
    
    Then to validate, run
      curl http://localhost:8080/kayaks

---
+++ src/main/java/com/okta/springbootjpa/KayakRepository.java
@@ -0,0 +1,8 @@
+package com.okta.springbootjpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface KayakRepository extends CrudRepository<Kayak, Integer> {
+}
